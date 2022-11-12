808954f8d59a686bf21ebc078049ce26291f2f91
lucenasoft <getugbr@gmail.com>
2022-11-12 12:03:05 -0300

    criação de dados no banco e modificação direta no admin-django

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from core.models import Evento
+
+
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('titulo','data_evento','data_criacao','usuario')
+
 # Register your models here.
+
+admin.site.register(Evento,EventoAdmin)
