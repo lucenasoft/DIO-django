64c7ec87974d45419dc5e59d88f17f6ac180b0c5
lucenasoft <getugbr@gmail.com>
2022-11-20 17:39:04 -0300

    trabalhando com views, rotas(url) e html para exibição

--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Evento
 
 # Create your views here.
+
+def lista_eventos(request):
+    evento = Evento.objects.all()
+    return render(request,'evento.html', context= {
+        'eventos': evento,
+    })
