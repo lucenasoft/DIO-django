45011a8bf10fb917be0683e1f9de4e5732220a29
lucenasoft <getugbr@gmail.com>
2022-12-27 13:48:51 -0300

    Adição de alteração e exclusão de evento.

--- agenda_dio/urls.py
+++ agenda_dio/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     path('login/submit', views.submit_login),
     path('agenda/evento/', views.evento),
     path('agenda/evento/submit', views.submit_evento),
+    path('agenda/evento/delete/<int:id_evento>/', views.delete_evento),
     path('logout/', views.logout_user),
     path('', RedirectView.as_view(url='agenda/')),
-
 ]
